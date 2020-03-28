7c5c8a4187944efa0fc80510782f31b0c8163cde
FerranVega <ferranvegacarol@gmail.com>
2020-03-28 21:40:14 +0000

    Clean GPA_percap file Couple updates on the GDP script

--- GDP per cap cleaning.R
+++ GDP per cap cleaning.R
@@ -3,3 +3,21 @@ rm(list = ls()) # clear environment
 library(tidyverse)
 
 GDPpc <- read.csv("GDP_percap_1990to2005.csv", head = TRUE, sep=";") 
+
+
+#Use borders data to extract clean list of country id's so that I can appropriately match nodes
+
+load(file = "borders.mat.rda") 
+countries <- colnames(borders.mat)
+countries <- countries[-1] 
+# Remove .na entry, which in borders.mat implies country has no adjacencies
+
+#We can create a new data frame with the row names of the adjacency matrix,
+#and match the variables you want as attributes to this new data frame:
+att <- data.frame('countryID' = as.character(countries))
+
+for (i in 1990:2005){
+  att$GDPi <- as.character(GDPpc$Xi[match(att$countries, dt$studentID)])
+}
+
+
