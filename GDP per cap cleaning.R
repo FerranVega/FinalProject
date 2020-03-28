131ec2b242d8c42ca164ee48160390a641c2565d
FerranVega <ferranvegacarol@gmail.com>
2020-03-28 21:45:16 +0000

    A couple more changes to GDP script Sorted data in Excel file and reuploaded it

--- GDP per cap cleaning.R
+++ GDP per cap cleaning.R
@@ -2,7 +2,7 @@
 rm(list = ls()) # clear environment
 library(tidyverse)
 
-GDPpc <- read.csv("GDP_percap_1990to2005.csv", head = TRUE, sep=";") 
+GDPpc <- read.csv("GDP_percap_1990to2005_clean.csv", head = TRUE, sep=";") 
 
 
 #Use borders data to extract clean list of country id's so that I can appropriately match nodes
@@ -10,6 +10,7 @@ GDPpc <- read.csv("GDP_percap_1990to2005.csv", head = TRUE, sep=";")
 load(file = "borders.mat.rda") 
 countries <- colnames(borders.mat)
 countries <- countries[-1] 
+countries <- sort(countries)
 # Remove .na entry, which in borders.mat implies country has no adjacencies
 
 #We can create a new data frame with the row names of the adjacency matrix,
