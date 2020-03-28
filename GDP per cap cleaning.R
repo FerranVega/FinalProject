269ca23e79b16139d5173b57a54b330a0ffce04c
FerranVega <ferranvegacarol@gmail.com>
2020-03-28 22:17:52 +0000

    Additional work... This attribute is almost ready!

--- GDP per cap cleaning.R
+++ GDP per cap cleaning.R
@@ -15,10 +15,24 @@ countries <- sort(countries)
 
 #We can create a new data frame with the row names of the adjacency matrix,
 #and match the variables you want as attributes to this new data frame:
-att <- data.frame('countryID' = as.character(countries))
+att <- data.frame('country' = as.character(countries))
+att$GDP1990 <- GDPpc$X1990[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1991 <- GDPpc$X1991[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1992 <- GDPpc$X1992[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1993 <- GDPpc$X1993[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1994 <- GDPpc$X1994[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1995 <- GDPpc$X1995[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1996 <- GDPpc$X1996[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1997 <- GDPpc$X1997[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1998 <- GDPpc$X1998[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP1999 <- GDPpc$X1999[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP2000 <- GDPpc$X2000[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP2001 <- GDPpc$X2001[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP2002 <- GDPpc$X2002[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP2003 <- GDPpc$X2003[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP2004 <- GDPpc$X2004[match(att$country, as.character(GDPpc$Country.Code))]
+att$GDP2005 <- GDPpc$X2005[match(att$country, as.character(GDPpc$Country.Code))]
 
-for (i in 1990:2005){
-  att$GDPi <- as.character(GDPpc$Xi[match(att$countries, dt$studentID)])
-}
+
 
 
