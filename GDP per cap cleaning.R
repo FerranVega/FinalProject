d4f0d0e14eb6adc88a7b696426eeeb676f3b6fa1
FerranVega <ferranvegacarol@gmail.com>
2020-04-05 16:27:58 +0100

    quick name change to att dataframe

--- GDP per cap cleaning.R
+++ GDP per cap cleaning.R
@@ -15,23 +15,23 @@ countries <- sort(countries)
 
 #We can create a new data frame with the row names of the adjacency matrix,
 #and match the variables you want as attributes to this new data frame:
-att <- data.frame('country' = as.character(countries))
-att$GDP1990 <- GDPpc$X1990[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1991 <- GDPpc$X1991[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1992 <- GDPpc$X1992[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1993 <- GDPpc$X1993[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1994 <- GDPpc$X1994[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1995 <- GDPpc$X1995[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1996 <- GDPpc$X1996[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1997 <- GDPpc$X1997[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1998 <- GDPpc$X1998[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP1999 <- GDPpc$X1999[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP2000 <- GDPpc$X2000[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP2001 <- GDPpc$X2001[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP2002 <- GDPpc$X2002[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP2003 <- GDPpc$X2003[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP2004 <- GDPpc$X2004[match(att$country, as.character(GDPpc$Country.Code))]
-att$GDP2005 <- GDPpc$X2005[match(att$country, as.character(GDPpc$Country.Code))]
+attGDPpc <- data.frame('country' = as.character(countries))
+attGDPpc$GDP1990 <- GDPpc$X1990[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1991 <- GDPpc$X1991[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1992 <- GDPpc$X1992[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1993 <- GDPpc$X1993[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1994 <- GDPpc$X1994[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1995 <- GDPpc$X1995[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1996 <- GDPpc$X1996[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1997 <- GDPpc$X1997[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1998 <- GDPpc$X1998[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP1999 <- GDPpc$X1999[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP2000 <- GDPpc$X2000[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP2001 <- GDPpc$X2001[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP2002 <- GDPpc$X2002[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP2003 <- GDPpc$X2003[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP2004 <- GDPpc$X2004[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
+attGDPpc$GDP2005 <- GDPpc$X2005[match(attGDPpc$country, as.character(GDPpc$Country.Code))]
 
 
 
