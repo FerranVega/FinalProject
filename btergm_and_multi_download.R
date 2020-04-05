e13f86233c6b2c197ba89d1aedabc93bd42ba964
Mohit Negi <mohitnegi1442000@gmail.com>
2020-04-05 12:56:04 +0530

    Uploaded Btergm installation script

---
+++ btergm_and_multi_download.R
@@ -0,0 +1,36 @@
+
+install.packages("devtools")
+install.packages("installr")
+library(installr)
+install.Rtools()
+library(devtools)
+devtools::install_github("tedhchen/multilayer.ergm", build_opts = c("--no-resave-data", "--no-manual"))
+
+
+install.packages("C:\Users\Mohit\Documents\R\win-library\3.6\btergm_1.9.4.tar", repos = NULL, type="source")
+
+
+url <- "https://cran.r-project.org/src/contrib/Archive/btergm/btergm_1.9.4.tar.gz"
+pkgFile <- "btergm_1.9.4.tar.gz"
+download.file(url = url, destfile = pkgFile)
+
+install.packages("xergm.common")
+
+install.packages(pkgs=pkgFile, type="source", repos=NULL)
+
+
+
+
+url <- "https://cran.r-project.org/src/contrib/Archive/xergm.common/xergm.common_1.7.7.tar.gz"
+pkgFile <- "xergm.common_1.7.7.tar.gz"
+download.file(url = url, destfile = pkgFile)
+
+install.packages("xergm.common")
+
+install.packages(pkgs=pkgFile, type="source", repos=NULL)"
+pkgFile <- "btergm_1.9.4.tar.gz"
+download.file(url = url, destfile = pkgFile)
+
+install.packages("xergm.common")
+
+install.packages(pkgs=pkgFile, type="source", repos=NULL)
