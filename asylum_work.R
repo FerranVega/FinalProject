56a56f1d428e457cbedbde470feab6abb5e40c73
Mohit Negi <mohitnegi1442000@gmail.com>
2020-04-28 13:19:09 +0530

    Now asylum_work has civil war attribute. All that's left is to run it and get new networks for years.

--- asylum_work.R
+++ asylum_work.R
@@ -2,71 +2,81 @@ library(tidyverse)
 library(lubridate)
 library(dils)
 library(statnet)
-library(data.table)
+library(plyr)
+################# LOAD ASYLUM DATA.
 
-################################################################ ASYLUM EDGELIST.
+
 
 #load("data90.rda")
-#load("data95.rda")
-load("data20.rda")
+load("data95.rda")
+#load("data20.rda")
 
-data20 <- subset(data20,data20$EventForm == "<HIDE>")
-#data95 <- subset(data95,data95$EventForm == "<HIDE>")
-#data90 <- subset(data90,data90$EventForm == "<HIDE>")
+load("civil95s.rda")
+
+data20 <- subset(data95,data95$EventForm == "<HIDE>")
 
 data20 <- dplyr::select(data20,"EventDate", "SrcName","TgtName")
-#data95 <- dplyr::select(data95,"EventDate", "SrcName","TgtName")
-#data90 <- dplyr::select(data90,"EventDate", "SrcName","TgtName")
+civil <- dplyr::select(civil,"EventDate","SrcName","TgtName")
 
-#data90$EventDate <- mdy(data90$EventDate)
-#data90 <- data90[order(data90$EventDate),]
-#data90 <- subset(data90,data90$EventDate < "1992/01/01") ###################### 1990 - 1992.
-
-data20$EventDate <- mdy_hms(data20$EventDate)
+data20$EventDate <- ymd(data20$EventDate)
+civil$EventDate <- ymd(civil$EventDate)
 data20 <- data20[order(data20$EventDate),]
-data20 <- subset(data20,data20$EventDate < "2005-01-01" & data20$EventDate >= "2004-01-01") ###################### 1990 - 1995.
+civil <- civil[order(civil$EventDate),]
 
-#data90 <- data90[,-1]
 
-#data90$SrcName <- as.character(data90$SrcName)
-#data90$TgtName <- as.character(data90$TgtName)
-#data90$counter <- c(rep(1,nrow(data90)))
+############## SPECIFY TIME PERIOD & MAKE THE ASYLUM EDGELIST.
+
+
+
+data20 <- subset(data20,data20$EventDate < "2000/01/01" & data20$EventDate >= "1999/01/01") ###################### 1990 - 1995.
+civil  <- subset(civil,  civil$EventDate < "2000/01/01" & civil$EventDate  >= "1999/01/01")
 
 data20 <- data20[,-1]
+civil <- civil[,-1]
 
 data20$SrcName <- as.character(data20$SrcName)
 data20$TgtName <- as.character(data20$TgtName)
 data20$counter <- c(rep(1,nrow(data20)))
 
-################################################################ CREATING OTHER EDGELISTS
+civil$SrcName <- as.character(civil$SrcName)
+civil$TgtName <- as.character(civil$TgtName)
+civil$counter <- c(rep(1,nrow(civil)))
+
+civil <- ddply(civil,.(SrcName,TgtName),nrow)
+civil <- civil[,-2]
+colnames(civil)[1] <- "country"
+
+##################################### CREATING OTHER EDGELISTS.
+
+
 
 load(file = "migrants_by_year.rda")
 load(file = "borders.mat.rda") 
 load(file = "wars_by_year.rda") 
 load(file = "ally_by_year.rda") 
 
-ally1990 <- ally_by_year[[15]]
+ally1990 <- ally_by_year[[10]]
+war1990 <- wars_by_year[[10]]
+migrants1990 <- migrants_by_year[[2]]
+
+#
 ally1990_1 <- ally1990
 temp <- ally1990_1[,1]
 ally1990_1[,1] <- ally1990_1[,2]
 ally1990_1[,2] <- temp
 ally1990 <- rbind(ally1990, ally1990_1) # Makes alliances edglist "complete"
-
-war1990 <- wars_by_year[[15]] # Already "complete" edgelist
-migrants1990 <- migrants_by_year[[3]] # Change every 5 years
-
-
-#mig <- graph.adjacency(as.matrix(migrants1990[,2:190]))
-#get.edgelist(mig)
+#
 
 colnames(borders.mat)[3] <- "UK_"
 rownames(borders.mat)[3] <- "UK_"
-borders.mat_without_NA <- as.matrix(borders.mat[-(1:2),-(1:2)])
-bord <- EdgelistFromAdjacency(borders.mat_without_NA, nodelist = colnames(borders.mat_without_NA))
-landlock <- borders.mat[,1]
 
 mig1990 <- EdgelistFromAdjacency(as.matrix(migrants1990[,2:190]), nodelist = colnames(migrants1990[,2:190]))
 
+borders.mat_without_NA <- as.matrix(borders.mat[-(1:2),-(1:2)])
+
+bord <- EdgelistFromAdjacency(borders.mat_without_NA, nodelist = colnames(borders.mat_without_NA))
+
+landlock <- borders.mat[,1]
 
 m1 <- merge(war1990[,-4],ally1990,all = TRUE)
 colnames(m1)[3] <- "Wars"
@@ -89,9 +99,6 @@ colnames(m3)[6] <- "Mig"
 colnames(data20)[1] <- "namea"
 colnames(data20)[2] <- "nameb"
 
-m3$namea <- as.character(m3$namea)
-m3$nameb <- as.character(m3$nameb)
-
 m4 <- merge(m3, data20, by = c("namea", "nameb"), all = TRUE)
 colnames(m4)[7] <- "Asylum"
 
@@ -104,6 +111,10 @@ attGDPpc$country <- as.character(attGDPpc$country)
 
 attGDPpc$country[attGDPpc$country == "_UK"] <- "UK_"
 node.att.1990 <- attGDPpc[,1:2] # Create GDP node att column
+
+node.att.1990 <- merge(node.att.1990, civil, by = "country", all = TRUE) #Merging civil war data.
+colnames(node.att.1990)[3] <- "CivilWars"
+node.att.1990$CivilWars[is.na(node.att.1990$CivilWars)] = 0
 
 #load(file = "HDI.mat.rda") 
 #attHDI$country <- as.character(attHDI$country)
@@ -142,19 +153,22 @@ edge.att.1990 <- edge.att.1990[order(edge.att.1990$namea, edge.att.1990$nameb),]
 #bord_adj_1990 <- AdjacencyFromEdgelist(edge.att.1990[,c(1:2,5)])
 #mig_adj_1990 <- AdjacencyFromEdgelist(edge.att.1990[,c(1:2,6)])
 asylum_adj_1990 <- AdjacencyFromEdgelist(edge.att.1990[,c(1:2,7)])
-
 #Adjlist <- list(war_adj_1990$adjacency,alliance_adj_1990$adjacency,bord_adj_1990$adjacency,mig_adj_1990$adjacency)
-
 #save(node.att.1990,file = "Asylum_Node_Attributes_1990.rda")
 #save(Adjlist,file = "Asylum_Edge_Adjacencies_1990.rda")
 #save(asylum_adj_1990,file = "Asylum_Adjacency_1990.rda")
 
 ################################################################ ERGM
 
+
+
 asylumnet <- network(asylum_adj_1990$adjacency,directed = TRUE,matrix.type = "adjacency")
 
 
+
+
 network::set.vertex.attribute(asylumnet, 'Per Capita Income', as.numeric(node.att.1990$GDP1990))
+network::set.vertex.attribute(asylumnet, 'Civil Conflicts', as.numeric(node.att.1990$CivilWars))
 #network::set.vertex.attribute(asylumnet, 'HDI', node.att.1990$HDI1990)
 #network::set.vertex.attribute(asylumnet, 'EFI', node.att.1990$EFI1990)
 #network::set.vertex.attribute(asylumnet, 'Religion', as.character(node.att.1990$major))
@@ -164,17 +178,6 @@ network::set.network.attribute(asylumnet,'Border', edge.att.1990$border)
 network::set.network.attribute(asylumnet,'Migrants', edge.att.1990$Mig)
 
 
-save(asylumnet, file = "asylumnet2004final.rda")
 
 
-
-
-
-#model_2 <- ergm(asylumnet ~ edges + mutual() + nodeocov('Per Capita Income')
-#                + nodeicov('Per Capita Income') + absdiff('Per Capita Income')
-#                + gwidegree(1,fixed = TRUE) + gwodegree(1, fixed = TRUE)
-#                + edgecov('Wars') + edgecov('Alliance')
-#                + edgecov('Border') + edgecov('Migrants')
-#                )
-#
-#summary(model_2)
+save(asylumnet, file = "asylumnet1999final.rda")
